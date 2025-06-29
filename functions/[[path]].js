0263a848ad67b94e69e9442cf44fd21df16698e5
Michal Ferber <michal.ferber@ari-integration.com>
2025-06-28 20:44:54 -0400

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -4,19 +4,20 @@ export async function onRequest(context) {
                      'Unknown';
     
     const ipList = clientIp.split(',').map(ip => ip.trim());
-    let ipv6 = ipList.find(ip => /:/.test(ip) && !/^\d+\.\d+\.\d+\.\d+$/.test(ip)) || 'No IPv6 detected';
+    let ipv6 = ipList.find(ip => /:/.test(ip) && !/^\d+\.\d+\.\d+\.\d+$/.test(ip)) || 
+               'No IPv6 address detected. This service requires an IPv6 connection. Use `curl -6` to force IPv6.';
   
     const { pathname } = new URL(context.request.url);
     if (pathname === '/' || pathname === '') {
-      return new Response(ipv6, {
-        status: 200,
-        headers: { 
-          'Content-Type': 'text/plain', 
-          'Cache-Control': 'no-store',
-          'Access-Control-Allow-Origin': '*' // Allow dev.ipcow.com
-        }
-      });
+        return new Response(ipv6, {
+            status: 200,
+            headers: { 
+                'Content-Type': 'text/plain', 
+                'Cache-Control': 'no-store',
+                'Access-Control-Allow-Origin': '*'
+            }
+        });
     }
   
     return context.next();
-  }
+}
